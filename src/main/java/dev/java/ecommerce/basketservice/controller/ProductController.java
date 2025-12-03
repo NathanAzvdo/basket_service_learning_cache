3e869f7bd167288c57cbccaa137218ecddd19885
NathanAzvdo <nathan.azevedo28@gmail.com>
2025-12-03 19:20:03 -0300

    feat: conexão com api feita

---
+++ src/main/java/dev/java/ecommerce/basketservice/controller/ProductController.java
@@ -0,0 +1,35 @@
+package dev.java.ecommerce.basketservice.controller;
+
+import dev.java.ecommerce.basketservice.client.response.PlatziProductResponse;
+import dev.java.ecommerce.basketservice.service.ProductService;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService){
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PlatziProductResponse>> getAllProducts(){
+        return ResponseEntity.ok(productService.getAllProducts());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PlatziProductResponse> getProductBy(@PathVariable Long id){
+        return ResponseEntity.ok(productService.getProductById(id));
+    }
+
+}
